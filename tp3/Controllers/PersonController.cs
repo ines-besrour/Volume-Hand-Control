d666bbf3b796e47cdeee04625d958b3994f00a96
ines-besrour <inesbes2001@gmail.com>
2022-12-03 11:53:00 +0100

    mon premier commit

---
+++ tp3/Controllers/PersonController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System;
+using tp3.Models;
+
+namespace tp3.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult View(int id)
+        {
+            Debug.WriteLine("ID: " + id.ToString());
+            Person? p = Personal_info.GetPerson(id);
+            if (p != null)
+            {
+                ViewBag.person = p;
+                return View();
+            }
+            ViewBag.error = "Not found";
+            return View("ErrorAction");
+        }
+
+        public IActionResult All()
+        {
+            List<Person> res = Personal_info.GetAllPerson();
+
+            return View(res);
+        }
+
+        public IActionResult View()
+        {
+            return View();
+        }
+    }
+}
---
+++ tp3/Models/Personal_info.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.SQLite;
+
+namespace tp3.Models
+{
+    public class Personal_info
+    {
+        private static readonly string dbConfig = "Data Source=C:\\Users\\heha\\OneDrive\\Desktop\\GL3\\tp3.db";
+        public static List<Person> GetAllPerson()
+        {
+            SQLiteConnection conn = new SQLiteConnection(dbConfig);
+            conn.Open();
+
+            SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM personal_info", conn);
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            List<Person> res = new List<Person>();
+            while (reader.Read())
+            {
+                Person tmp = new Person(reader.GetInt32(0));
+                tmp.firstName = reader.GetString(1);
+                tmp.lastName = reader.GetString(2);
+                tmp.email = reader.GetString(3);
+                tmp.dateBirth = reader.GetString(4);
+                tmp.image = reader.GetString(5);
+                tmp.country = reader.GetString(6);
+
+                res.Add(tmp);
+            }
+
+            conn.Close();
+            return res;
+        }
+        public static Person? GetPerson(int id)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(dbConfig))
+            {
+                conn.Open();
+                SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM personal_info where id=@id", conn);
+                cmd.Parameters.Add(new SQLiteParameter("@id", id));
+
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    Person res = new Person(id);
+                    res.firstName = reader.GetString(1);
+                    res.lastName = reader.GetString(2);
+                    res.email = reader.GetString(3);
+                    res.dateBirth = reader.GetString(4);
+                    res.image = reader.GetString(5);
+                    res.country = reader.GetString(6);
+                    return res;
+                }
+            }
+            return null;
+
+        }
+    }
+}
