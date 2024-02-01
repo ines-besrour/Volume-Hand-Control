4c2f46a28722bac49c254b6521ff5b7787b844f2
ines-besrour <inesbes2001@gmail.com>
2024-02-01 15:30:10 +0100

    setup

--- tp3/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace tp3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
